8e229e9bb97118867e2a1bc47931654821cd501d
Abdul3030 <developerjahidul100@gmail.com>
2021-08-16 21:25:08 -0700

    Added suppl. class test

--- component/Supplement/Fraction.tsx
+++ component/Supplement/Fraction.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
+import { Field } from 'formik';
+import { gradColPallate } from '../../colorPallate';
 
 
 
-
-const Fraction = () => {
+const Fraction = ({name, options, values, prevQId, showResults, answer}) => {
     
     const [selected, setSelected] = React.useState<number>(null);
+    let isActive = prevQId === null ? true : false;
+    let selectedColor = gradColPallate[0];
+    if(showResults){
+        selectedColor = selected === answer ? gradColPallate[3] : gradColPallate[2];
+    }
+    if(prevQId){
+        values[prevQId] ? isActive = true : isActive = false;
+    }
 
     return (
-        <div className="w-1/2 mx-auto my-5 text-pro-gray flex justify-center text-2xl items-center">
+        <div className={`relative w-1/2 mx-auto my-5 text-pro-gray flex justify-center text-2xl items-center}`}>
             {/* ******************* Question **************** */}
+            <div className={`absolute w-full h-full top-0 left-0 bg-opacity-70 bg-white z-10 ${!isActive ? 'block' : 'hidden'}`}></div>
+            <div className={`absolute w-full h-full top-0 left-0 z-10 ${showResults ? 'block' : 'hidden'}`}></div>
             <div className="flex justify-center items-center px-10 py-5 shadow-pro rounded">
                 <div className="">
                     <span className="inline-block text-center ">
@@ -29,39 +40,40 @@ const Fraction = () => {
                     </span>
                 </div>
             </div>
-            {/* ****************** Equalt ****************** */}
-            <div className="mx-5">
+            {/* ****************** Equal ****************** */}
+            <div className="mx-5 flex items-center">
                 <h1 className=""> = </h1>
             </div>
             {/* ********************* Possible Answers **************** */}
-            <div className={`p-0.5 bg-gradient-to-br rounded shadow-pro mr-5 ${selected === 0 ? 'from-pro-blue to-pro-lightblue' : 'from-white to-white'}`}>
-                <div onClick={() => setSelected(0)} className={`bg-white rounded p-5 cursor-pointer `}>
-                    <span className="inline-block text-center ">
-                        <sup className="border-b-2 border-pro-gray p-2 block font-semibold">13</sup>
-                                {/* <span className="hidden">&frasl;</span> */}
-                        <sub className="block font-semibold">20</sub>
-                    </span>
-                </div>
-            </div>
-            <div className={`p-0.5 bg-gradient-to-br rounded shadow-pro mr-5 ${selected === 1 ? 'from-pro-blue to-pro-lightblue' : 'from-white to-white'}`}>
-                <div onClick={() => setSelected(1)} className={`bg-white rounded p-5 cursor-pointer `}>
-                    <span className="inline-block text-center">
-                        <sup className="border-pro-gray border-b-2 p-2 block font-semibold">13</sup>
-                        <span className="hidden">&frasl;</span>
-                        <sub className="block font-semibold">20</sub>
-                    </span>
-                </div>
-            </div>
-            <div className={`p-0.5 bg-gradient-to-br rounded shadow-pro mr-5 ${selected === 2 ? 'from-pro-blue to-pro-lightblue' : 'from-white to-white'}`}>
-                <div onClick={() => setSelected(2)} className={`bg-white rounded p-5 cursor-pointer `}>
-                        <span className="inline-block text-center ">
-                            <sup className="border-b-2 border-pro-gray p-2 block font-semibold">5</sup>
-                                    <span className="hidden">&frasl;</span>
-                            <sub className="block font-semibold">10</sub>
-                        </span>
-                </div>
-            </div>
-            
+            {/* New Code */}
+            <Field name={name}>
+                {
+                    ({field}) => {
+                        return options.map((option, idx) => {
+                            const fraction = option.split('/');
+                            console.log(fraction);
+                            return (
+                                <div key={idx} className={`p-0.5 bg-gradient-to-br rounded shadow-pro mr-5 ${selected === option ? selectedColor : 'from-white to-white'}`}>
+                                    <label htmlFor={option} onClick={() => setSelected(option)} className={`w-20 h-full bg-white rounded p-5 cursor-pointer flex justify-center items-center`}>
+                                        <div className="inline-block text-center cursor pointer">
+                                            <sup className="cursor-pointer border-b-2 border-pro-gray p-2 block font-semibold">{fraction[0]}</sup>
+                                                <span className="hidden">&frasl;</span>
+                                            <sub className="cursor-pointer block font-semibold">{fraction[1]}</sub>
+                                        </div>
+                                        <input 
+                                            className="appearance-none"
+                                            type="radio" 
+                                            id={option}
+                                            {...field} 
+                                            value={option}
+                                            />
+                                    </label>
+                                </div>
+                            )
+                        })
+                    }
+                }
+            </Field>
         </div>
     )
 };
--- pages/supplementary/[subjectId]/[classId].tsx
+++ pages/supplementary/[subjectId]/[classId].tsx
@@ -5,23 +5,91 @@ import { BsPlayFill } from 'react-icons/bs';
 import {useTimer} from 'use-timer';
 import { gradColPallate } from '../../../colorPallate';
 import Fraction from '../../../component/Supplement/Fraction';
+import { Formik, Form, Field } from 'formik';
 import { useState } from 'react';
 import Congratulation from '../../../component/Supplement/Congratulation';
 
-
+const testData = [
+    {
+        id: 458210,
+        type: 'fragment',
+        question: '12/13+15/12',
+        options: [
+            '7/12',
+            '9/11',
+            '8/5',
+            '10/17'
+        ],
+        answer: '8/5'
+    },
+    {
+        id: 458211,
+        type: 'fragment',
+        question: '12/13+15/12',
+        options: [
+            '5/15',
+            '9/14',
+            '8/10',
+            '18/25'
+        ],
+        answer: '8/10'
+    },
+    {
+        id: 458212,
+        type: 'fragment',
+        question: '12/13+15/12',
+        options: [
+            '9/35',
+            '2/3',
+            '1/8',
+            '3/11'
+        ],
+        answer: '9/35'
+    },
+    {
+        id: 458213,
+        type: 'fragment',
+        question: '12/13+15/12',
+        options: [
+            '8/65',
+            '3/12',
+            '8/16',
+            '10/25'
+        ],
+        answer: '10/25'
+    },
+    {
+        id: 458214,
+        type: 'fragment',
+        question: '12/13+15/12',
+        options: [
+            '6/18',
+            '5/14',
+            '1/9',
+            '2/13'
+        ],
+        answer: '2/13'
+    },
+];
 const ClassTest = () => {
     const router = useRouter();
 
     const [congrats, setCongrats] = useState(false);
     const [showResults, setShowResults] = useState(false);
+    const [results, setResults] = useState({});
+    const [correctAnswer, setCorrectAnswer] = useState(null);
+
     const submitRef = React.useRef(null);
-    console.log(congrats);
+    console.log(results);
+
+    // Stop watch for test
     const { time, start, pause, reset, status } = useTimer({
         initialTime: 600,
         endTime: 0,
         timerType: 'DECREMENTAL',
         onTimeOver: () => {
-          submitRef.current.click();
+        submitRef.current.click();
+        // start();
         },
       });
 
@@ -41,10 +109,12 @@ const ClassTest = () => {
         seconds = Math.floor((time % minute));
 
     
-    const onSubmitHanlder = (e) => {
-        e.preventDefault();
+    const onSubmitHanlder = (values) => {
         pause();
         setCongrats(true);
+        setResults(values);
+        const correctAns = testData.filter(question => question.answer === values[question.id]);
+        setCorrectAnswer(correctAns);
     };
 
     // After 5 minutes of submitting result will be shown and congrats window disappeared
@@ -56,6 +126,8 @@ const ClassTest = () => {
         return () => clearTimeout(timeout);
     },[showResults, congrats]);
 
+    
+    console.log(correctAnswer);
     return (
 
         <div className="relative w-full px-2 pt-1 select-none">
@@ -68,9 +140,14 @@ const ClassTest = () => {
                 </h1>
                 <div className="flex pt-5 items-center">
                     <div className="w-36">
-                        <h1 className={`text-5xl font-medium bg-clip-text bg-gradient-to-r text-transparent ${minutes < 6 && minutes >= 1 ? gradColPallate[1] : minutes < 1 ? gradColPallate[2] : gradColPallate[0]}`}>
+                       { 
+                       !showResults ?
+                       <h1 className={`text-5xl font-medium bg-clip-text bg-gradient-to-r text-transparent ${minutes < 6 && minutes >= 1 ? gradColPallate[1] : minutes < 1 ? gradColPallate[2] : gradColPallate[0]}`}>
                             {`${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`}
-                        </h1>
+                        </h1> :
+                        <h1 className={`text-5xl font-medium bg-clip-text bg-gradient-to-r text-transparent ${gradColPallate[0]}`}>
+                            {`${correctAnswer.length}/${testData.length}`}
+                        </h1>}
                     </div>
                     <div className="">
                         <h1 className="text-xs font-medium text-pro-darkgray">Supplementary Class 1</h1>
@@ -78,14 +155,46 @@ const ClassTest = () => {
                     </div>
                 </div>
             </div>
-            <form onSubmit={onSubmitHanlder} className="w-full px-8 scrollable overflow-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-pro-scroll">
+
+            {/* <FormikTesting /> */}
+            <div className="w-full px-8 scrollable overflow-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-pro-scroll">
+                <Formik
+                    initialValues={{
+                        458213: '',
+                        458214: '',
+                        458210: '',
+                        458211: '',
+                        458212: ''
+                    }}
+                    onSubmit={onSubmitHanlder}
+                >
+                    {
+                        ({values}) => (
+                            <Form>
+                                
+                                {
+                                    testData.map( (item, idx) => <Fraction key={item.id} showResults={showResults} answer={item.answer} prevQId={idx > 0 ? testData[idx-1].id : null} values={values} name={item.id} options={item.options} />)
+                                }
+                                {
+                                !showResults ?
+                                <div className="w-full my-5 flex justify-center items-center text-white">
+                                    <button ref={submitRef} className="bg-gradient-to-r from-pro-blue hover:from-pro-lightblue to-pro-lightblue hover:to-pro-blue rounded-lg px-10 py-1" type="submit">Submit</button>
+                                </div> : ''
+                                }
+                                
+                            </Form>
+                        )
+                    }
+                </Formik>
                 {
-                    Array(10).fill('').map((item, idx) => <Fraction key={idx} />)
+                    showResults ?
+                    <div className="w-full flex justify-center items-center text-white font-medium text-lg my-10">
+                        <button onClick={() => router.push('/supplementary')} className="bg-gradient-to-r from-pro-green hover:from-pro-spring to-pro-spring hover:to-pro-green rounded-md px-10 py-1 mx-5" >Other Classes</button>
+                        <button onClick={() => router.reload()} className="bg-gradient-to-r from-pro-blue hover:from-pro-lightblue to-pro-lightblue hover:to-pro-blue rounded-md px-10 py-1 mx-5">Redo Class</button>
+                    </div> : ''
                 }
-                <div className="w-full my-5 flex justify-center items-center text-white">
-                    <button ref={submitRef} className="bg-gradient-to-br from-pro-blue to-pro-lightblue rounded-lg px-10 py-1" type="submit">Submit</button>
-                </div>
-            </form>
+            </div>
+
             {
                 congrats && <Congratulation classTitle="Supplementary Class 1" />
             }
